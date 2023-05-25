66a5e0cbf25691de5d56416aa4ff31bfeddc0cce
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-25 18:03:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-socios
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-ioj2rq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
